c1ecbdd63058c52b9cbc8b5e82a9686278c56f13
abdessamad-zgor <abdessamadzgor@gmail.com>
2023-02-19 16:33:02 +0100

    try

--- api/admin-login.ts
+++ api/admin-login.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { getAuth } from "firebase-admin/auth";
 import { initializeApp } from "firebase-admin";
 import { cert, ServiceAccount } from "firebase-admin/app";
-import adminAccess from "../store/exp-911-firebase-adminsdk-6oxi3-d78a41bd05.json";
+import adminAccess from "./exp-911-firebase-adminsdk-6oxi3-d78a41bd05.json";
 
 let auth = getAuth(
   initializeApp({
